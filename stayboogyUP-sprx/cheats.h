4f2d2fad342b2ebccf463783bfc54143a73d450f
root <root@DESKTOP-FDMLTH5.localdomain>
2022-11-30 21:15:59 -0600

    God Mode perfected
    add different versions

--- stayboogyUP-sprx/cheats.h
+++ stayboogyUP-sprx/cheats.h
@@ -1,60 +1,61 @@
+//cheats.h
+
 void UAV()
 {
-  char uav[] = { 0x10 };
-  //write_process(0x18DB880, uav, sizeof(uav));
-  PS3::WriteBytes(0x18DB880, uav, sizeof(uav));
-  
-  char radar[] = { 0x02 };
-  //write_process(0x5F067, radar, sizeof(radar));
-  PS3::WriteBytes(0x5F067, radar, sizeof(radar));
+	char uav[] = { 0x10 };
+	PS3::WriteBytes(0x18DB880, uav, sizeof(uav));
+
+	char radar[] = { 0x02 };
+	PS3::WriteBytes(0x5F067, radar, sizeof(radar));
 }
 
 void Perks()
 {
-  char perks[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-  //write_process(0x01227788, perks, sizeof(perks));
-  PS3::WriteBytes(0x01227788, perks, sizeof(perks));
+	char perks[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+	PS3::WriteBytes(0x01227788, perks, sizeof(perks));
 }
 
 void Godly()
 {
-  char godly[] = "11111";
-  PS3::WriteString(0x012272ea, godly);
+	char godly[] = "81";
+	PS3::WriteString(0x012272ea, godly);
+}
+
+void Manly()
+{
+	char manly[] = "80";
+	PS3::WriteString(0x012272ea, manly);
 }
 
 void Loaded()
 {
-  char loaded[] = { 0x00, 0x90 };
-  //write_process(0x01227631, loaded, sizeof(loaded));
-  //write_process(0x01227649, loaded, sizeof(loaded));
-  //write_process(0x012276cd, loaded, sizeof(loaded));
-  //write_process(0x012276d9, loaded, sizeof(loaded));
-  //write_process(0x012276b5, loaded, sizeof(loaded));
-  //write_process(0x012276c1, loaded, sizeof(loaded));
-  //write_process(0x012276d9, loaded, sizeof(loaded));
-  //write_process(0x012276e5, loaded, sizeof(loaded));
-  //write_process(0x012326f9, loaded, sizeof(loaded));
-  //write_process(0x01232711, loaded, sizeof(loaded));
-  //write_process(0x01232771, loaded, sizeof(loaded));
-  //write_process(0x01232795, loaded, sizeof(loaded));
-  //write_process(0x0123277d, loaded, sizeof(loaded));  
-  //write_process(0x01232789, loaded, sizeof(loaded)); 
-  //write_process(0x012327a1, loaded, sizeof(loaded)); 
-  //write_process(0x012327ad, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x01227631, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x01227649, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276cd, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276d9, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276b5, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276c1, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276d9, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012276e5, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x012326f9, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x01232711, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x01232771, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x01232795, loaded, sizeof(loaded));
-  PS3::WriteBytes(0x0123277d, loaded, sizeof(loaded));  
-  PS3::WriteBytes(0x01232789, loaded, sizeof(loaded)); 
-  PS3::WriteBytes(0x012327a1, loaded, sizeof(loaded)); 
-  PS3::WriteBytes(0x012327ad, loaded, sizeof(loaded));
+	char loaded[] = { 0x00, 0x90 };
+	PS3::WriteBytes(0x01227631, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x01227649, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276cd, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276d9, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276b5, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276c1, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276d9, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012276e5, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012326f9, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x01232711, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x01232771, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x01232795, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x0123277d, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x01232789, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012327a1, loaded, sizeof(loaded));
+	PS3::WriteBytes(0x012327ad, loaded, sizeof(loaded));
 };
+//various ways to do the same thing:
+
+//write_process(0x012327ad, loaded, sizeof(loaded));
+
+//write_process(0x01227788, perks, sizeof(perks));
+
+//write_process(0x5F067, radar, sizeof(radar));
+
+//write_process(0x5F067, radar, sizeof(radar));
+
+//write_process(0x18DB880, uav, sizeof(uav));
+
